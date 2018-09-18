88bad860883c4008a36a2c03b247ba030479150f
Gguidini <giovanni.guidini@gmail.com>
2018-09-18 13:52:11 -0300

    Adding  test for class method.

--- Trabalho 1/test/test_job.rb
+++ Trabalho 1/test/test_job.rb
@@ -2,10 +2,19 @@ require 'test/unit'
 require 'time'
 require_relative "../src/Job"
 
+=begin
+    Test Cenarios for Job class. Each test is a different method of the class.
+    Class definition in ../src/Job.rb
+=end
+
 class JobTest < Test::Unit::TestCase    
     def setup
         @job_aux = Job.new("Auxiliar", 1500)
         @job_tech = Job.new("Técnico", 3000)
+    end
+
+    def test_class_method
+        assert_equal(2, Job.get_quantity_Jobs())
     end
 
     def test_get_name
