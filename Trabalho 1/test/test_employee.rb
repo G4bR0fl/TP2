2e7e8352c29de5f68ef479d491535baca5dbb21f
verasthiago <thiago.veras.machado@gmail.com>
2018-10-04 03:28:51 +0000

    Added get formation integration test & I simplified conditional in sector line 34

--- Trabalho 1/test/test_employee.rb
+++ Trabalho 1/test/test_employee.rb
@@ -44,9 +44,9 @@ class TestIntegrationTest < Test::Unit::TestCase
     def setup
         @job_1 = Job.new('Auxiliar', 1500)
         @job_2 = Job.new('Tecnico', 3000)
-        @employee_1 = Employee.new(name: 'Jose', birth_date: Time.parse('25/03/1997'), cpf: '111.111.111-00', join_date: Time.parse('10/02/2005'), bonus: 350.00, role: @job_1)
-        @employee_2 = Employee.new( name: 'Thiago', birth_date: Time.parse('07/07/1998'), cpf: '222.222.222-00', join_date: Time.parse('31/11/2010'), bonus: 350.00, role: @job_2)
-        @employee_3 = Employee.new( name: 'Amanda', birth_date: Time.parse('20/09/1994'), cpf: '333.333.333-00', join_date: Time.parse('20/02/2003'), bonus: 500.00, role: @job_1)
+        @employee_1 = Employee.new(name: 'Jose', birth_date: Time.parse('25/03/1997'), cpf: '111.111.111-00', join_date: Time.parse('10/02/2005'), bonus: 350.00, role: @job_1, formation:["Administrador", "Gestor de Pessoas"])
+        @employee_2 = Employee.new( name: 'Thiago', birth_date: Time.parse('07/07/1998'), cpf: '222.222.222-00', join_date: Time.parse('31/11/2010'), bonus: 350.00, role: @job_2, formation:["Programador", "Direito"])
+        @employee_3 = Employee.new( name: 'Amanda', birth_date: Time.parse('20/09/1994'), cpf: '333.333.333-00', join_date: Time.parse('20/02/2003'), bonus: 500.00, role: @job_1, formation:[])
     end
 
     def test_get_salary
@@ -81,5 +81,11 @@ class TestIntegrationTest < Test::Unit::TestCase
         assert_raise ArgumentError do
             @employee_2.set_Job("Batata")
         end
+    end
+
+    def test_get_formation
+        assert_equal(["Administrador", "Gestor de Pessoas"], @employee_1.get_formation())
+        assert_equal(["Programador", "Direito"], @employee_2.get_formation())
+        assert_equal([], @employee_3.get_formation())
     end
 end
