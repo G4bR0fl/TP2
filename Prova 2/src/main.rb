cb0c1668130c50692cc8d77ab737e2c909cebd47
Vitor Dullens <vitordullens2.0@gmail.com>
2018-12-02 23:19:18 -0200

    acertando a funcao agora

--- Prova 2/src/main.rb
+++ Prova 2/src/main.rb
@@ -3,15 +3,14 @@ require_relative "Solution"
 # calcular o sistema com n iteracoes
 sistema = Solution.new(100)
 # calcula o sistema1: valores x0: 0.1, y0: 0.1 e z0: 0
-sistema1 = sistema.calculate(xi: 0.1, yi: 0.1, zi: 0)
-# calcula o sistema1: valores x0: 0.1, y0: 0.1 e z0: 0.1
-sistema2 = sistema.calculate(xi: 0.1, yi: 0.1, zi: 0.1)
-
+sistema1 = sistema.calculate(xn: 0.1, yn: 0.1, zn: 0)
 puts "sistema1:"
 puts "x100: " + sistema1[0].to_s
 puts "y100: " + sistema1[1].to_s
 puts "z100: " + sistema1[2].to_s
 
+# calcula o sistema1: valores x0: 0.1, y0: 0.1 e z0: 0.1
+sistema2 = sistema.calculate(xn: 0.1, yn: 0.1, zn: 0.1)
 puts "sistema2:"
 puts "x100: " + sistema2[0].to_s
 puts "y100: " + sistema2[1].to_s
