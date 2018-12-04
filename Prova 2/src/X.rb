8d8c8320ef8c0c7f83a746408f9327cdb6df0c6b
GabrielBessa <gabrielbessarofl@gmail.com>
2018-12-04 08:26:16 -0200

    final version

--- Prova 2/src/X.rb
+++ Prova 2/src/X.rb
@@ -6,7 +6,7 @@ require_relative "Function"
 class X < Function
     def run(xn, yn, zn)
         def f(xn)
-            @f_const = {m0: 0.7879, m1:-1.4357, Bp:1}
+            @f_const = {m0: -0.7879, m1:-1.4357, Bp:1}
             resp = @f_const[:m0] * xn + 0.5 * (@f_const[:m1] - @f_const[:m0]) * ((xn + @f_const[:Bp]).abs - (xn - @f_const[:Bp]).abs)
             return resp
         end
